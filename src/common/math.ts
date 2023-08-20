7509e947f0ad04a1b1f8b684eb2f62ed449d5f70
Jani Nykänen <jnyknn@gmail.com>
2023-08-20 17:44:39 +0300

    Slightly improved terrain generation & rendering

--- src/common/math.ts
+++ src/common/math.ts
@@ -4,3 +4,6 @@ export const negMod = (m : number, n : number) : number => ((m % n) + n) % n;
 
 
 export const clamp = (x : number, min : number, max : number) : number => Math.max(Math.min(x, max), min);
+
+
+export const sampleUniform = (min : number, max : number) : number => min + ( (Math.random() * (max - min + 1)) | 0 );
--- src/game/terrain.ts
+++ src/game/terrain.ts
@@ -1,18 +1,29 @@
+import { negMod, sampleUniform } from "../common/math.js";
 import { AssetManager } from "../core/assets.js";
 import { ProgramEvent } from "../core/event.js";
 import { Bitmap } from "../renderer/bitmap.js";
 import { Canvas } from "../renderer/canvas.js";
 
 
+
+const enum GroundType {
+
+    Gap = 0,
+    Bridge = 1,
+    Ground = 2
+};
+
+
 export class Terrain {
 
 
-    private groundLayer : number[];
-    private bridge : boolean[];
+    private groundHeight : number[];
+    private groundType : GroundType[];
+    private activeHeight : number = 0;
+    private activeType : GroundType = GroundType.Gap;
 
     private groundTimer : number = 1;
     private groundSlopeTimer : number = 0;
-    private groundHeight : number = 0;
     private groundMode : boolean = false;
     private bridgeEnabled : boolean = false;
 
@@ -28,77 +39,104 @@ export class Terrain {
         this.width = ((event.screenWidth / 16) | 0) + 1;
         this.height = (event.screenHeight / 16) | 0;
 
-        this.groundLayer = (new Array<number> (this.width)).fill(-1);
-        this.bridge = (new Array<boolean> (this.width)).fill(false);
+        this.groundHeight = (new Array<number> (this.width)).fill(-1);
+        this.groundType = (new Array<GroundType> (this.width)).fill(GroundType.Gap);
     }
+
+
+    private getGroundType = (x : number) : GroundType => this.groundType[negMod(x, this.width)];
 
 
     private updateGroundGenerator() : void {
 
         const GROUND_MIN = 1;
-        const GROUND_VARY = 3;
+        const GROUND_MAX = 4;
 
-        const GROUND_LENGTH_MIN = [2, 2];
-        const GROUND_LENGTH_VARY = [4, 8];
+        const TIMER_MIN = [2, 3, 2];
+        const TIMER_MAX = [4, 6, 8];
+
+        const NEW_GROUND_TYPE = [2, 2, 0];
 
         const BRIDGE_PROB = 0.25;
 
+        let oldType = this.activeType;
+
         if ((-- this.groundTimer) <= 0) {
 
-            this.groundMode = !this.groundMode;
-            if (this.groundMode) {
+            this.activeType = NEW_GROUND_TYPE[oldType as number];
+            this.bridgeEnabled = this.activeType == GroundType.Gap && Math.random() < BRIDGE_PROB;
+            if (this.bridgeEnabled) {
 
-                if (!this.bridgeEnabled) {
-
-                    this.groundHeight = this.height - (GROUND_MIN + ((Math.random()*(GROUND_VARY + 1)) | 0));
-                }
-                this.bridgeEnabled = false;
-            }
-            else {
-
-                this.bridgeEnabled = Math.random() < BRIDGE_PROB;
-                if (!this.bridgeEnabled) {
-
-                    this.groundHeight = -1;
-                }
+                this.activeType = GroundType.Bridge;
             }
 
-            this.groundTimer = GROUND_LENGTH_MIN[Number(this.groundMode)] + 
-                ((Math.random()*(GROUND_LENGTH_VARY[Number(this.groundMode)] + 1)) | 0);
+            this.groundType[this.tilePointer] = this.activeType;
+
+            if (this.activeType == GroundType.Ground && 
+                oldType == GroundType.Gap) {
+
+                this.activeHeight = this.height - sampleUniform(GROUND_MIN, GROUND_MAX);
+            }
+
+            this.groundTimer = sampleUniform(
+                TIMER_MIN[this.activeType as number], 
+                TIMER_MAX[this.activeType as number]);
         }
-        
-        this.groundLayer[this.tilePointer] = this.groundHeight;
-        this.bridge[this.tilePointer] = !this.groundMode && this.bridgeEnabled;
+
+        this.groundHeight[this.tilePointer] = this.activeHeight;
+        this.groundType[this.tilePointer] = this.activeType;
     }
 
 
-    private drawGroundLayer(canvas : Canvas, bmp : Bitmap | undefined) : void {
+    private drawgroundHeight(canvas : Canvas, bmp : Bitmap | undefined) : void {
 
         let dx : number;
         let dy : number;
         let i : number;
+
+        let sx : number;
 
         for (let x = 0; x < this.width; ++ x) {
 
             i = (x + this.tilePointer) % this.width;
 
             dx = x*16 - this.tileOffset;
-            dy = this.groundLayer[i];
+            dy = this.groundHeight[i];
 
-            if (dy == -1) 
-                continue;
-            
-            if (this.bridge[i]) {
+            switch (this.groundType[i]) {
 
+            // Gap
+            case 0:
+                break;
+
+            // Bridge
+            case 1:
                 canvas.drawBitmap(bmp, dx, dy*16, 96, 32, 16, 16);
-                continue;
-            }
+                break;
 
-            for (let y = dy; y < this.height; ++ y) {
+            // Ground
+            case 2:
 
-                canvas.drawBitmap(bmp, 
-                    dx, y*16, 
-                    32, (y == dy ? 0 : 1)*16, 16, 16);
+                for (let y = dy; y < this.height; ++ y) {
+
+                    sx = 32;
+                    if (this.getGroundType(i - 1) != GroundType.Ground) {
+
+                        sx = 16;
+                    }
+                    else if (this.getGroundType(i + 1) != GroundType.Ground) {
+
+                        sx = 48;
+                    }
+
+                    canvas.drawBitmap(bmp, 
+                        dx, y*16, 
+                        sx, (y == dy ? 0 : 1)*16, 16, 16);
+                }
+                break;
+
+            default:
+                break;
             }
         }
     }
@@ -120,6 +158,6 @@ export class Terrain {
 
         const bmpTerrain = assets.getBitmap("terrain");
     
-        this.drawGroundLayer(canvas, bmpTerrain);
+        this.drawgroundHeight(canvas, bmpTerrain);
     } 
 }
